4fc6589511c7a6f19fb733bb8b396e169ec581f3
Kartik Bholla <kartikbholla@Kartiks-MacBook-Pro.local>
2016-05-14 02:44:24 -0700

    5th - Partial Parse

--- app/src/main/java/com/example/kartikbholla/fourfp/MainActivity.java
+++ app/src/main/java/com/example/kartikbholla/fourfp/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.kartikbholla.fourfp;
 import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
+
+    public String syntax; //input string will be taken here
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +20,64 @@ public class MainActivity extends AppCompatActivity {
         if ( actionBar != null)
             actionBar.hide();
 
+        final Button run = (Button) findViewById(R.id.run);
+        final EditText input = (EditText) findViewById(R.id.input);
+        run.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v == run) {
+                    syntax = input.getText().toString();
+                    //syntax.length();
+                    Parse(syntax, syntax.length());
+                }
+            }
+
+
+        });
+
+
+        //int i = Integer.parseInt(myText.getText())
+
     }
+    public static void Parse(String syntax, int strlen){
+        //int index;
+        String lexem = null;
+        char letter;
+        //int newIndex;
+        int count = 0;
+        //boolean space = false;
+
+        for (int j=0; j<= strlen; j++){
+            if(syntax.charAt(j) == ' '||syntax.charAt(j) == '\t'){
+                count ++;
+                if (count == 1){
+                    vectorize(lexem);
+                }
+
+                // space = true;
+            }
+
+            else if(!(syntax.charAt(j) == ' '||syntax.charAt(j) == '\t')) {
+                letter = syntax.charAt(j);
+                lexem = lexem + letter;
+                count = 0;
+
+                //space = false;
+                // if(space == false);
+                // space=0;
+                // index = j;// non space caracter index
+                // newIndex = ParseV2(syntax, strlen, index);
+            }
+            else if (syntax.charAt(j) == '#'){
+                do {
+                    j++;
+                }while(syntax.charAt(j) == '\n');
+            }
+        }
+    }   // end of parse
+    public static void vectorize(String lexem){
+        Vector<String> lexemVector = null;
+        lexemVector.add(lexem);
+    }   // end of vectorize
+
 }
